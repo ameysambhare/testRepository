9f11113c9463402378de23d3b311c912c73a9798
ameysambhare <amey.sambhare@gmail.com>
2020-06-14 10:59:43 -0500

    Add files via upload

---
+++ UserInput.java
@@ -0,0 +1,12 @@
+package com.input;
+
+import java.util.Scanner;
+
+public class UserInput {
+	
+	public String getUserInput() {
+		Scanner sc = new Scanner(System.in);
+		return sc.next();
+	}
+
+}
